03f1bd35d0a549870bdc9e8e77edf38a3552ca49
Corey Andalora <corey.andalora@coxautoinc.com>
2020-04-02 08:58:21 -0400

    added Goal

--- src/main/java/singlesort/component/Goal.java
+++ src/main/java/singlesort/component/Goal.java
@@ -1,4 +1,16 @@
 package singlesort.component;
 
+import java.awt.*;
+
 public class Goal {
+
+    private Cardboard[] targets;
+
+    public Goal(Cardboard ... targets) {
+        this.targets = targets;
+    }
+
+    public Cardboard[] getTargets() {
+        return targets;
+    }
 }
